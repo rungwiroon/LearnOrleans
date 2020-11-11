75d3404a04fb77946d697533c44ff452f5d4569a
Rungwiroon Komalitipong <rungwiroon@wecomics.in.th>
2020-11-11 09:40:29 +0700

    Create project

---
+++ src/GrainInterfaces/IDeposit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrainInterfaces
+{
+    interface IDeposit : Orleans.IGrainWithIntegerKey
+    {
+        Task<decimal> Deposit(decimal amount);
+    }
+}
